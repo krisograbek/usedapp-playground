c4236edc7dc3f3c4809a4d954f23afe3bb026a1f
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-19 07:00:46 +0200

    prepare toggle state, change text and class depending on the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [isConnected, setIsConnected] = useState(false);
   return (
     <div className='container'>
       <h1>My App</h1>
-      <button className="button-disconnect">
-        Click Me
+      <button
+        className={`button ${!isConnected ? 'connect' : 'disconnect'}`}
+        onClick={() => setIsConnected(prevState => !prevState)}
+      >
+        {!isConnected ? "Connect" : "Disconnect"}
       </button>
     </div>
   );
