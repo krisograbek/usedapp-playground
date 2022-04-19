7bf32cecf732f87b79524810f89553cd087cce3b
krisograbek <krzysztof.ograbek@gmail.com>
2022-04-19 06:41:57 +0200

    initial setup, add simple button styles

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='container'>
+      <h1>My App</h1>
+      <button className="button-disconnect">
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
